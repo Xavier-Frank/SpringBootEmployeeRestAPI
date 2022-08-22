ef74d76cf08fc1127c5367d185b2423b5ea9452d
XavierFrank <oduorfrancis134@gmail.com>
2022-08-22 13:23:13 +0300

    Swagger ui description

--- src/main/java/com/xavi/EmployeeMicroservice/swagger/SwaggerConfiguration.java
+++ src/main/java/com/xavi/EmployeeMicroservice/swagger/SwaggerConfiguration.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Author: oduorfrancis134@gmail.com;
@@ -18,29 +19,36 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  **/
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfiguration implements WebMvcConfigurer {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.xavi.EmployeeMicroservice"))
                 .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    /**
+     *  Describe the swagger ui
+     * @return apiInfo()
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Spring Boot Employee Microservice REST Application with CRUD operations")
+                .description("Documentation for Employee Microservice REST Endpoints in Spring Boot + MySQL + HATEOAS" +
+                        "  " + " RabbitMQ + Tests").version("1.0")
+                .contact(new Contact("Author: Xavier Francis", " https://github.com/Xavier-Frank", "oduorfrancis134" +
+                        "@gmail.com"))
                 .build();
     }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars");
-    }
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-                .title("SPRING BOOT EMPLOYEE REST APPLICATION")
-                .description("A REST Application for Employee CRUD Operations")
-                .license("oduorfrancis134@gmail.com")
-                .version("1.0").build();
     }
 }
